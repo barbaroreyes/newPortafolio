0e91445160bb89d8b8d3653f08b728fc4157b21e
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-11-18 22:25:41 -0600

    added Austin map

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 document.querySelector('#home').addEventListener('click',(e) => {
    const ps = document.createElement('p')
-   ps.textContent = 'Hi there ';
+   ps.textContent = 'sdsdsds'
    ps.style = "border:1px solid red ; background : rgb(251, 190, 139); width : 300px; font-size: 30px" 
    document.querySelector('#sect').appendChild(ps)
 
 })
 document.querySelector('#about').addEventListener('click',(e) => {
     const ps = document.createElement('p')
-    ps.textContent = 'Lorem ipsum dolor sit amet consectetur'
-    ps.style = "border:1px solid red ; background : rgb(251, 190, 139); width : 300px; font-size: 30px" 
+    ps.textContent = 'sdsds'
+    ps.style = "border:1px solid red ; background : rgb(251, 190, 139)"
     document.querySelector('#sec').appendChild(ps)
  
  })
+ document.querySelector('#projects').addEventListener('click',(e) => {
+    const ps = document.createElement('p')
+    ps.textContent = 'sdsds'
+    ps.style = "border:1px solid red ; background : rgb(251, 190, 139)"
+    document.querySelector('ol').appendChild(ps)
+ 
+ })
+
 document.querySelector('#reset').addEventListener('click', (e)=> {
     const pss = document.querySelectorAll('p')
     pss.forEach(p => p.remove()) 
 })
+
+
  
 
 
---
+++ map.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Map() {
+  return (
+    <div>
+      <img alt='' src='https://www.google.com/maps/vt/data=5P4ONVexKCVOBj0TGShKJUHHEh92mCYp-iiYNH_ToCyE8lC8bKwnonJSUN8d9dMOASkgYIV0qOpY3MUAayvO3A7tQNMsawDb5NRKT6lbg5B71CZnmDhT0aA1rBBk9vKekI1JNuqLmj1SQgfPUx0lvmqOPTcg8Z4V3WCHXdacXOuQsphQpEnVjuv7R1HRi1FcOCG1Xr1g7ryrq2hclzzfAMf3UakE-PxCA9l0RJ7kvrwB_KbL3ZEzIZg'/>
+    </div>
+  )
+}
+
