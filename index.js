16edbf7c13c0d6d982a78b7cd491081c6cd191df
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-11-19 02:12:34 -0600

    update css

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-document.querySelector('#home').addEventListener('click',(e) => {
-   const ps = document.createElement('p')
-   ps.textContent = 'Hi there'
-   ps.style = "border:1px solid red ; background : rgb(251, 190, 139); width : 300px; font-size: 30px" 
-   document.querySelector('#sect').appendChild(ps)
+// document.querySelector('#home').addEventListener('click',(e) => {
+//    const ps = document.createElement('p')
+//    ps.textContent = 'Hi there'
+//    ps.style = "border:1px solid red ; background : rgb(251, 190, 139); width : 300px; font-size: 30px" 
+//    document.querySelector('#sect').appendChild(ps)
 
-})
+// })
 
-document.querySelector('#reset').addEventListener('click', (e)=> {
-    const pss = document.querySelectorAll('p')
-    pss.forEach(p => p.remove()) 
-})
+// document.querySelector('#reset').addEventListener('click', (e)=> {
+//     const pss = document.querySelectorAll('p')
+//     pss.forEach(p => p.remove()) 
+// })
 
 
  
